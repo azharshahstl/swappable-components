8386dfd1ee387f42a9e0c0b5f0df902e05b4ed6c
Azhar Shah <azharshahsaintlouis@gmail.com>
2021-06-22 11:10:32 -0500

    Still working on JS

--- src/Inputs/RadioInput.js
+++ src/Inputs/RadioInput.js
@@ -8,19 +8,19 @@ const RadioInput = (props) => {
             <div className={classes.RadioDiv} >
             <div className={classes.Question}>What is 2 + 2?</div>
             <div className={classes.Inputs}>
-                <input type="radio" id="3" name="number3" value="3"/>
+                <input type="radio" id="3" name="answer" value="3"/>
                 <label htmlFor="3">3</label>
             </div>
             <div className={classes.Inputs}>
-                <input type="radio" id="4" name="number4" value="4"/>
+                <input type="radio" id="4" name="answer" value="4"/>
                 <label htmlFor="4">4</label>
             </div>
             <div className={classes.Inputs}>
-                <input type="radio" id="1" name="number1" value="1"/>
+                <input type="radio" id="1" name="answer" value="1"/>
                 <label htmlFor="1">1</label>
             </div>
             <div className={classes.Inputs} >
-                <input type="radio" id="5" name="number5" value="5" />
+                <input type="radio" id="5" name="answer" value="5" />
                 <label htmlFor="5">5</label>
             </div>
         </div>
--- src/Inputs/SwitchInput.js
+++ src/Inputs/SwitchInput.js
@@ -5,15 +5,25 @@ const SwitchInput = (props) => {
 
     const [disable, setDisable] = useState("true")
 
+    const handleSwitchOnClick = () => {
+        setDisable((!disable).toString());
+    }
+
+    const handleButtOnClick = (event) => {
+        event.preventDefault();
+        setDisable("true");
+        alert("Do you live in the US button has been clicked");
+    }
+
     return (
-        <div className={classes.Container}>
+        <div className={classes.Container} onClick={handleSwitchOnClick}>
             <div>
                 <label className={classes.Switch}>
                 <input type="checkbox"/>
                 <span className={classes.SliderRound}></span>
                 </label>   
             </div>
-            <button className={classes.ResQuestion} disable={disable}>
+            <button className={classes.ResQuestion} disable={disable} onClick={handleButtOnClick}>
                 Do you live in the US?
             </button>
         </div>
