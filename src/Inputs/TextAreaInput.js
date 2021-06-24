5a19de5644506d4230463430a218ab1d14afda27
Azhar Shah <azharshahsaintlouis@gmail.com>
2021-06-24 08:29:34 -0500

    removing commented out code

--- src/Inputs/TextAreaInput.js
+++ src/Inputs/TextAreaInput.js
@@ -3,32 +3,8 @@ import classes from './TextAreaInput.module.css'
 
 const TextAreaInput = (props) => {
 
-    // const [text, setText] = useState("");
-    // const [requireMessage, setRequireMessage] = useState("")
-    // const [typing, setTyping] = useState("");
-
-    // const handleOnBlur = () => {
-    //     if (text.length === 0) {
-    //         setRequireMessage("Required");         
-    //     }
-    // }
-
-    // const handleOnChange = (event) => {
-    //     setText(event.target.value);
-    //     if (event.target.value.length > 0) {
-    //         setRequireMessage("");
-    //         setTyping("true");
-            
-    //     } else {
-    //         setTyping("false");
-    //     }
-    // }
-
-    // // const handleOnKeyUp = () => {
-    // //     setCounter(counter + 1);
-    // // }
-
     let checkForSuccess;
+
     if (props.success === "success"){
         checkForSuccess = <>
         <input type="text" className={classes.Success} 
@@ -51,7 +27,7 @@ const TextAreaInput = (props) => {
         onChange={props.inputOnChange} />
         <p className={classes.FailureMessage}>You have failed me for the last time.</p>
         </>
-    } else if (props.success === "default") {
+    } else if (props.success === "default" || props.success === "typing") {
         checkForSuccess = <input type="text" className={classes.TAInput} 
         typing={props.typing}
         id="answer"
@@ -59,16 +35,7 @@ const TextAreaInput = (props) => {
         value={props.inputValue}
         onBlur={props.inputBlur}
         onChange={props.inputOnChange} />
-    } else if (props.success === "typing"){
-        checkForSuccess = <input type="text" className={classes.TAInput} 
-        typing={props.typing}
-        id="answer"
-        placeholder="Your answer"
-        value={props.inputValue}
-        onBlur={props.inputBlur}
-        onChange={props.inputOnChange} />
-
-    }
+    } 
 
     return (
         <div className={classes.TADiv}>
@@ -87,24 +54,5 @@ const TextAreaInput = (props) => {
             </div>
         </div>
     )
-
-    // return (
-    //     <div className={classes.TADiv}>
-    //         <p className={classes.Required}>{props.textAreaRequiredMessage}</p>
-    //         {props.textAreaRequiredMessage ? <label htmlFor="textarea" maxLength="250" className={classes.AddAstericks}>Tell me something about yourself</label>
-    //          : <label htmlFor="textarea" maxLength="250" className={classes.TALabel}>Tell me something about yourself</label>}
-    //         <textarea id="textarea"
-    //          placeholder="Hint text" 
-    //          className={classes.TAInput}
-    //          typing={props.typing}
-    //          value={props.inputValue}
-    //          onChange={props.inputOnChange}
-    //          onBlur={props.inputBlur}/>
-    //         <div className={classes.Counter}>
-    //             <span>{props.inputValue.length}</span>
-    //             <span>/ 250</span>
-    //         </div>
-    //     </div>
-    // )
 }
 export default TextAreaInput;
--- src/Inputs/TextInput.js
+++ src/Inputs/TextInput.js
@@ -4,27 +4,8 @@ import classes from './TextInput.module.css';
 
 const TextInput = (props) => {
 
-    // const [typing, setTyping] = useState("false");
-    // const [value, setValue] = useState("");
-    // const [requireMessage, setRequireMessage] = useState("")
-
-    // const handleOnBlur = () => {
-    //     if (value.length === 0) {
-    //         setRequireMessage("Required");         
-    //     }
-    // }
-
-    // const handleOnChange = (event) => {
-    //     setValue(event.target.value); 
-    //     if (event.target.value.length > 0) {
-    //         setRequireMessage("");
-    //         setTyping("true");
-            
-    //     } else {
-    //         setTyping("false");
-    //     }
-    // }
     let checkForSuccess;
+    
     if (props.success === "success"){
         checkForSuccess = <>
         <input type="text" className={classes.Success} 
@@ -70,20 +51,5 @@ const TextInput = (props) => {
             {checkForSuccess}
         </div>
     )
-    // else {
-    //     return(
-    //         <div className={classes.InputDiv}>
-    //         <label htmlFor="answer" className={classes.InputLabel}>
-    //          What is the meaning of life?
-    //         </label>
-    //         <input type="text" className={classes.Input} 
-    //         id="answer"
-    //         placeholder="Your answer"
-    //         value={value}
-    //         onBlur={handleOnBlur}
-    //         onChange={handleOnChange} />
-    //     </div> 
-    //     )
-    // }
  }
 export default TextInput;
